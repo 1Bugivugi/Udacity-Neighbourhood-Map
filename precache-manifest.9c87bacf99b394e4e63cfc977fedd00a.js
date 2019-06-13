c7787c8be98c4f2ff218ecddc1db1e16e8f1faec
Gleb Rezoun <gleb.rezun@gmail.com>
2019-06-13 14:54:24 +0300

    Updates

---
+++ precache-manifest.9c87bacf99b394e4e63cfc977fedd00a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "946b02d95c311f385891",
+    "url": "/Udacity-Neighbourhood-Map/static/js/runtime~main.946b02d9.js"
+  },
+  {
+    "revision": "8f0423a19e7395140922",
+    "url": "/Udacity-Neighbourhood-Map/static/js/main.8f0423a1.chunk.js"
+  },
+  {
+    "revision": "8a5cb4bd01a2e15a107b",
+    "url": "/Udacity-Neighbourhood-Map/static/js/1.8a5cb4bd.chunk.js"
+  },
+  {
+    "revision": "8f0423a19e7395140922",
+    "url": "/Udacity-Neighbourhood-Map/static/css/main.d81db951.chunk.css"
+  },
+  {
+    "revision": "7d9417e32bf0e79353f309dd50dc44c5",
+    "url": "/Udacity-Neighbourhood-Map/index.html"
+  }
+];
